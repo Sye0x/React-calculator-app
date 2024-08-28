9e417dd8a1022493dfea7646d1f7aac8d72c5e75
unknown <s.r.mahamid@gmail.com>
2024-08-28 06:05:04 +0500

    second commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,10 @@
 import { Text, View } from "react-native";
 import Buttons from "@/component/buttons";
+import Textarea from "@/component/Textarea";
 export default function Index() {
   return (
     <View>
+      <Textarea />
       <Buttons />
     </View>
   );
--- component/buttons.tsx
+++ component/buttons.tsx
@@ -19,69 +19,71 @@ const Buttons = () => {
   }
 
   return (
-    <View>
-      <View style={{ flexDirection: "row" }}>
-        {buttons("c").map((number, index) => (
-          <Pressable style={styles.container2}>
-            <Text
-              style={{ fontSize: 22, fontWeight: "600", color: "white" }}
-              key={index}
-            >
-              {number}
-            </Text>
-          </Pressable>
-        ))}
+    <View style={{ flexDirection: "row", marginTop: 10 }}>
+      <View>
+        <View style={{ flexDirection: "row" }}>
+          {buttons("c").map((number, index) => (
+            <Pressable style={styles.container2}>
+              <Text
+                style={{ fontSize: 22, fontWeight: "600", color: "white" }}
+                key={index}
+              >
+                {number}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          {buttons(7).map((number, index) => (
+            <Pressable style={styles.container}>
+              <Text style={styles.text} key={index}>
+                {number}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          {buttons(4).map((number, index) => (
+            <Pressable style={styles.container}>
+              <Text style={styles.text} key={index}>
+                {number}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+        <View style={{ flexDirection: "row" }}>
+          {buttons(1).map((number, index) => (
+            <Pressable style={styles.container}>
+              <Text style={styles.text} key={index}>
+                {number}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+
+        <View style={{ flexDirection: "row" }}>
+          {buttons(0).map((number, index) => (
+            <Pressable style={styles.container}>
+              <Text style={styles.text} key={index}>
+                {number}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      </View>
+      <View>
         <Pressable style={styles.container2}>
-          <Text style={{ fontSize: 22, fontWeight: "600", color: "white" }}>
-            <Feather name="delete" size={24} color="white" />
-          </Text>
+          <Feather name="delete" size={24} color="white" />
         </Pressable>
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        {buttons(7).map((number, index) => (
-          <Pressable style={styles.container}>
-            <Text style={styles.text} key={index}>
-              {number}
-            </Text>
-          </Pressable>
-        ))}
         <Pressable style={styles.container2}>
-          <Text style={{ fontSize: 22, fontWeight: "600", color: "white" }}>
-            -
-          </Text>
+          <Text style={styles.text2}>-</Text>
         </Pressable>
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        {buttons(4).map((number, index) => (
-          <Pressable style={styles.container}>
-            <Text style={styles.text} key={index}>
-              {number}
-            </Text>
-          </Pressable>
-        ))}
         <Pressable style={styles.container2}>
-          <Text style={{ fontSize: 22, fontWeight: "600", color: "white" }}>
-            +
-          </Text>
+          <Text style={styles.text2}>+</Text>
         </Pressable>
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        {buttons(1).map((number, index) => (
-          <Pressable style={styles.container}>
-            <Text style={styles.text} key={index}>
-              {number}
-            </Text>
-          </Pressable>
-        ))}
-      </View>
-      <View style={{ flexDirection: "row" }}>
-        {buttons(0).map((number, index) => (
-          <Pressable style={styles.container}>
-            <Text style={styles.text} key={index}>
-              {number}
-            </Text>
-          </Pressable>
-        ))}
+        <Pressable style={styles.container3}>
+          <Text style={styles.text2}>=</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -95,9 +97,9 @@ const styles = StyleSheet.create({
 
     backgroundColor: "#FAF9F6",
     padding: 10,
-    height: 70,
-    width: 70,
-    borderRadius: 35,
+    height: 90,
+    width: 90,
+    borderRadius: 45,
     shadowColor: "#333333",
     shadowOffset: {
       width: -2,
@@ -113,9 +115,9 @@ const styles = StyleSheet.create({
 
     backgroundColor: "#8A9A5B",
     padding: 10,
-    height: 70,
-    width: 70,
-    borderRadius: 35,
+    height: 90,
+    width: 90,
+    borderRadius: 45,
     shadowColor: "#333333",
     shadowOffset: {
       width: -2,
@@ -126,9 +128,31 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  container3: {
+    margin: 10,
 
+    backgroundColor: "#8A9A5B",
+    padding: 10,
+    height: 200,
+    width: 90,
+    borderRadius: 45,
+    shadowColor: "#333333",
+    shadowOffset: {
+      width: -2,
+      height: 4,
+    },
+    shadowOpacity: 0.4,
+    shadowRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   text: {
     fontSize: 22,
     fontWeight: "600",
+  },
+  text2: {
+    fontSize: 22,
+    fontWeight: "600",
+    color: "white",
   },
 });
